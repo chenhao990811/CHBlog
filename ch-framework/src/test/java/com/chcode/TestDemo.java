3983f98b66de16da7e30e2dd654d4d9f51f165be
chenhao <ch990811@163.com>
2023-02-05 12:23:46 +0800

    master fifth commit alter personal info

--- ch-framework/src/test/java/com/chcode/TestDemo.java
+++ ch-framework/src/test/java/com/chcode/TestDemo.java
@@ -32,5 +32,6 @@ public class TestDemo {
         System.out.println("master分支，测试合并冲突");
         System.out.println("代码已上传GitHub，在GitHub中修改代码，测试拉取远程库中的代码");
         System.out.println("配置了码云仓库，测试push");
+        System.out.println("修改了码云个人空间地址，测试push");
     }
 }
